2551ae8383a60db123c19c6fee97841e0be4d24f
FelipheGomez <feliphegomez@gmail.com>
2019-07-10 15:16:08 -0500

    V1.0.0

---
+++ errors.php
@@ -0,0 +1,16 @@
+<?php 
+global $display_error;
+$display_error = array();
+// Error 401: No autorizado
+$display_error[401] = new stdclass();
+$display_error[401]->code = 401;
+$display_error[401]->error = "Unauthorized";
+$display_error[401]->message = "Esta página no funciona, Si el problema persiste, ponte en contacto con el propietario del sitio web.";
+
+
+function showError($num_e){
+	global $display_error;
+	$error = new stdclass();
+	return json_encode($display_error[$num_e]);
+}
+
